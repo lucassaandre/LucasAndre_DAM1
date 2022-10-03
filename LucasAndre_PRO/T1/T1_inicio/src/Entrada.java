50c4b69fdef4ee71afd8c785c554db088857dc4d
Lucas Andrè <lucasandreb@hotmail.com>
2022-10-03 22:40:58 +0200

    03-10-2022

--- LucasAndre_PRO/T1/T1_inicio/src/Entrada.java
+++ LucasAndre_PRO/T1/T1_inicio/src/Entrada.java
@@ -1,7 +1,7 @@
-public class Entrada {
-
-    public static void main(String[]arg){
+public class Entrada
+{
+    public static void main(String[] args)
+    {
         System.out.println("Hola Mundo");
     }
-
 }
