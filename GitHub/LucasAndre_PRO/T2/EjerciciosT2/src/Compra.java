c773d9b24f4bb4a1f54f52eb600b2460ef5af5ed
Lucas Andrè <109322415+lucassaandre@users.noreply.github.com>
2022-10-07 13:57:14 +0200

    07-10-2022

--- GitHub/LucasAndre_PRO/T2/EjerciciosT2/src/Compra.java
+++ GitHub/LucasAndre_PRO/T2/EjerciciosT2/src/Compra.java
@@ -10,6 +10,11 @@ public class Compra
         Scanner lectura = new Scanner(System.in);
         System.out.println("Introduzca el valor de su compra con IVA: ");
         int     compra = lectura.nextInt();
+        System.out.println("Introduzca el % valor del IVA aplicado: ");
+        int     iva = lectura.nextInt();
+
+        double  ivaCompra = (compra * iva) / 100;
+        double  compraSinIva = ;
 
     }
 }
