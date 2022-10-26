689c1850f24d1776ed070156bd9b279f31edc043
Lucas Andrè <109322415+lucassaandre@users.noreply.github.com>
2022-10-26 14:29:33 +0200

    26-10-2022

---
+++ LucasAndre_PRO/EjerciciosT3/src/Ejercicio6.java
@@ -0,0 +1,36 @@
+/*public class Ejercicio6
+{
+    public static void main(String[] args)
+    {
+        int numero1 = 20;
+        int numero2 = 20;
+        int numero3 = 20;
+        int numero4 = 20;
+
+        if (condicionNumero(numero1) && condicionNumero(numero2)
+            && condicionNumero(numero3) && condicionNumero(numero4))
+        {
+            double inversoUno = 0.0, inversoDos = 0.0, inversoTres = 0.0, inversoCuatro = 0.0;
+            inversoUno = condicionNumero(numero1);
+        }
+        else
+        {
+            System.out.println("Alguno no está en rango");
+        }
+    }
+    private static boolean condicionNumero(int numero)
+    {
+        return numero >= -100 && numero < 100;
+    }
+    private static double calculoInverso(int numero)
+    {
+        if (numero != 0)
+        {
+            return  (double)1/numero;
+        }
+        else
+        {
+            return 0.0;
+        }
+    }
+}*/
---
+++ LucasAndre_PRO/EjerciciosT3/src/PruebaFloat.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PruebaFloat
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Altura: ");
+
+        float   altura = input.nextFloat();
+        System.out.println(altura);
+    }
+}
