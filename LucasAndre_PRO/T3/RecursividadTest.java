87776180fb52646c7a6bc237afe529642db54dbe
Lucas Andrè <109322415+lucassaandre@users.noreply.github.com>
2022-11-09 14:34:38 +0100

    09-11-2022

---
+++ LucasAndre_PRO/T3/RecursividadTest.java
@@ -0,0 +1,20 @@
+public class RecursividadTest
+{
+    public static void main(String[] args)
+    {
+        int num1 = 1000;
+	    int r;
+	    r = num1;
+	    //numbers(r);
+        System.out.println(r);
+    }
+    private static void numbers(int r)
+    {
+        while(r >= 10)
+        {
+            numbers(r/10);
+            numbers(r%10);
+            System.out.println(r);
+        }
+    }
+}
