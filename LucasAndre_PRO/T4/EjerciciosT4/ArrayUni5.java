f2aee8d03f89c474bec83d66b4696e766bb536e0
Lucas Andrè <lucasandreb@hotmail.com>
2022-11-30 18:34:16 +0100

    2

--- LucasAndre_PRO/T4/EjerciciosT4/ArrayUni5.java
+++ LucasAndre_PRO/T4/EjerciciosT4/ArrayUni5.java
@@ -4,7 +4,28 @@ public class ArrayUni5
 {
     public static void main(String[] args)
     {
-        System.out.println("Introduce la longitud para dos arrays de enteros: ");
-        int longArray = new Scanner(System.in).nextInt();
+        System.out.println("Introduce la longitud para 2 arrays de enteros: ");
+        int[] arrayUno = new int[new Scanner(System.in).nextInt()];
+        int[] arrayDos = new int[arrayUno.length];
+
+        for (int i = 0; i < arrayUno.length; i++)
+        {
+            System.out.println("Introduce el "+(i+1)+"º número del primer array: ");
+            arrayUno[i] = new Scanner(System.in).nextInt();   
+            System.out.println("Introduce el "+(i+1)+"º número del segundo array: ");
+            arrayDos[i] = new Scanner(System.in).nextInt();   
+        }
+        //creación y relleno de arrayTres con suma de posiciones de arrayUno y arrayDos
+        int[] arrayTres = new int[arrayUno.length];
+        for (int i = 0; i < arrayTres.length; i++)
+        {
+            arrayTres[i] = arrayUno[i] + arrayDos[i];    
+        }
+        System.out.printf("Números arrayTres: ");
+        for (int i : arrayTres)
+        {
+            System.out.printf("%d, ", i);    
+        }
+        System.out.println();
     }    
 }
---
+++ LucasAndre_PRO/T4/EjerciciosT4/ArrayUni6.java
@@ -0,0 +1,6 @@
+public class ArrayUni6
+{
+    public static void main(String[] args) {
+        
+    }    
+}
