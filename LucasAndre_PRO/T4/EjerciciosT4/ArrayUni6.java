a2233fbca5dc5d5138c9c1414b6b05d5e630ff46
Lucas Andrè <lucasandreb@hotmail.com>
2022-11-30 19:32:16 +0100

    3

--- LucasAndre_PRO/T4/EjerciciosT4/ArrayUni6.java
+++ LucasAndre_PRO/T4/EjerciciosT4/ArrayUni6.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class ArrayUni6
 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+    {
+        System.out.println("Introduce la longitud de tu array: ");    
+        int[] array = new int[new Scanner(System.in).nextInt()];
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.println("Introduce el número "+(i+1)+" de tu array: ");
+            array[i] = new Scanner(System.in).nextInt();    
+        }
+        System.out.println("Array creado: ");
+        for (int i : array)
+        {
+            System.out.printf("%d, ", i);
+        }
+        System.out.println();
+        System.out.println("Array desplazado hacia la derecha: ");
+        int primero = array[0];
+        int aux;
+        for (int i = array.length - 1; i > 0; i--)
+        {
+            if(i > 0)
+            {
+                aux = array[i];
+                array[i] = array[i-1];
+                array[i-1] = aux; 
+                System.out.printf("%d, ", array[i]);
+            }
+        }
     }    
 }
