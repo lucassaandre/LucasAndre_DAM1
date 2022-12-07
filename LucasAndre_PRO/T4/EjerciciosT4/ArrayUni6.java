1302ed443279db7b44c430560389afbd6be61df3
Lucas Andrè <lucasandreb@hotmail.com>
2022-12-07 19:01:19 +0100

    7-12-22

--- LucasAndre_PRO/T4/EjerciciosT4/ArrayUni6.java
+++ LucasAndre_PRO/T4/EjerciciosT4/ArrayUni6.java
@@ -4,26 +4,50 @@ public class ArrayUni6
 {
     public static void main(String[] args)
     {
-        System.out.println("Introduce la longitud de tu array: ");    
-        int[] array = new int[new Scanner(System.in).nextInt()];
-        for (int i = 0; i < array.length; i++)
+        System.out.println("Introduzca la longitud de su array: ");
+        int[] myArray = new int[new Scanner(System.in).nextInt()];
+        for (int i = 0; i < myArray.length; i++)
         {
-            System.out.println("Introduce el número "+(i+1)+" de tu array: ");
-            array[i] = new Scanner(System.in).nextInt();    
+            System.out.println("Introduzca el número "+(i+1)+" de su array: ");
+            myArray[i] = new Scanner(System.in).nextInt();
         }
         System.out.println("Array creado: ");
-        for (int i : array)
+        for (int i : myArray)
         {
-            System.out.printf("%d, ", i);
+            System.out.printf("%d, ", i);    
         }
         System.out.println();
-        System.out.println("Array desplazado hacia la izq: ");
-        int primero = array[0];
+        arrayDer(myArray);
+        arrayIzq(myArray);
+    }
+    public static void arrayDer(int[] array)
+    {
+        System.out.println("Array desplazado hacia la derecha: ");
+        int auxDer = array[array.length - 1];
+        for (int i = array.length - 1; i >= 1; i--)
+        {
+            array[i] = array[i-1];
+        }
+        array[0] = auxDer;
+        for (int i : array)
+        {
+            System.out.printf("%d, ", i);    
+        }
+        System.out.println();
+    }
+    public static void arrayIzq(int[] array)
+    {
+        System.out.println("Array desplazado hacia la izquierda: ");
+        int auxIzq = array[0];
         for (int i = 0; i < array.length - 1; i++)
         {
-            array[i] = array[i+1];
-            System.out.printf("%d, ", array[i]);
+            array[i] = array[i + 1];
         }
-        System.out.printf("%d\n", primero);
-    }    
+        array[array.length - 1] = auxIzq;
+        for (int i : array)
+        {    
+            System.out.printf("%d, ", i);    
+        }
+        System.out.println();
+    }
 }
